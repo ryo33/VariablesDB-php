c92053c11d9cd12be8e1e0d9f2f1840d4a76686d
ryo33 <ryo12redstone@gmail.com>
2015-08-10 16:20:41 +0900

    Enable to work

--- VarDB.php
+++ VarDB.php
@@ -15,39 +15,132 @@ class VarDB{
         $this->space = $space;
     }
 
-    function setup(){
-        $ddb = new DiffDB($this->con);
+    public function setup($ddb){
         $ddb->addTable($this->prefix . 'variable', [
             'id' => 'INTEGER PRIMARY KEY AUTO_INCREMENT',
+            'space' => 'VARCHAR(32) NOT NULL',
             'name' => 'VARCHAR(32) NOT NULL',
             'type' => 'SMALLINT UNSIGNED',
             'created' => 'INTEGER'
         ]);
         $ddb->addTable($this->prefix . 'integer', [
             'id' => 'INTEGER PRIMARY KEY AUTO_INCREMENT',
+            'space' => 'VARCHAR(32) NOT NULL',
             'variable_id' => 'INTEGER',
-            'integer' => 'INTEGER NULL',
+            'value' => 'INTEGER NULL',
             'created' => 'INTEGER'
         ]);
         $ddb->addTable($this->prefix . 'float', [
             'id' => 'INTEGER PRIMARY KEY AUTO_INCREMENT',
+            'space' => 'VARCHAR(32) NOT NULL',
             'variable_id' => 'INTEGER',
-            'float' => 'FLOAT NULL',
+            'value' => 'FLOAT NULL',
             'created' => 'INTEGER'
         ]);
         $ddb->addTable($this->prefix . 'string', [
             'id' => 'INTEGER PRIMARY KEY AUTO_INCREMENT',
+            'space' => 'VARCHAR(32) NOT NULL',
             'variable_id' => 'INTEGER',
-            'string' => 'VARCHAR(256) NULL',
+            'value' => 'VARCHAR(256) NULL',
             'created' => 'INTEGER'
         ]);
         $ddb->addTable($this->prefix . 'boolean', [
             'id' => 'INTEGER PRIMARY KEY AUTO_INCREMENT',
+            'space' => 'VARCHAR(32) NOT NULL',
             'variable_id' => 'INTEGER',
-            'boolean' => 'BOOLEAN NULL',
+            'value' => 'BOOLEAN NULL',
             'created' => 'INTEGER'
         ]);
         $ddb->updateDB(['drop'=>true, 'truncate'=>true]);
+    }
+
+    private function get_database($type){
+        switch($type){
+        case self::T_INTEGER:
+            return 'integer';
+        case self::T_FLOAT:
+            return 'float';
+        case self::T_STRING:
+            return 'string';
+        case self::T_BOOLEAN:
+            return 'boolean';
+        }
+    }
+
+    private function cast($type, $value){
+        switch($type){
+        case self::T_INTEGER:
+            return (int)$value;
+        case self::T_FLOAT:
+            return (float)$value;
+        case self::T_STRING:
+            return $value;
+        case self::T_BOOLEAN:
+            return (bool)$value;
+        }
+    }
+
+    private function get_type($value){
+        if(is_int($value)) return self::T_INTEGER;
+        else if(is_float($value)) return self::T_FLOAT;
+        else if(is_bool($value)) return self::T_BOOLEAN;
+        else return self::T_STRING;
+    }
+
+    private function get_variable($name){
+        $result = $this->con->fetch('SELECT COUNT(`id`), `id`, `type` FROM `' . $this->prefix . 'variable` WHERE `space` = ? AND `name` = ?', [$this->space, $name]);
+        if($result['COUNT(`id`)'] === '1'){
+            return $result;
+        }
+        return null;
+    }
+
+    function set_space($space){
+        $this->space = $space;
+    }
+
+    function set($name, $value){
+        $variable = $this->get_variable($name);
+        $type = $this->get_type($value);
+        if($variable !== null){
+            if($type === (int)$variable['type']){
+                $this->con->update($this->prefix . $this->get_database((int)$variable['type']), ['value' => $value],
+                    '`space` = ? AND `variable_id` = ?', [$this->space, $variable['id']]);
+            }else{
+                $this->con->execute('DELETE FROM `' . $this->prefix . $this->get_database((int)$variable['type']) . '` WHERE `space` = ? AND `variable_id` = ?',
+                    [$this->space, $variable['id']]);
+                $this->con->update($this->prefix . 'variable', ['type' => $type], '`id` = ?', [$variable['id']]);
+                $this->con->insert($this->prefix . $this->get_database($type),
+                    ['space' => $this->space, 'variable_id' => $variable['id'], 'value' => $value]);
+            }
+        }else{
+            $variable_id = $this->con->insert($this->prefix . 'variable', ['space' => $this->space, 'name' => $name, 'type' => $type], true);
+            $this->con->insert($this->prefix . $this->get_database($type),
+                ['space' => $this->space, 'variable_id' => $variable_id, 'value' => $value]);
+        }
+    }
+
+    function get($name, $default=null){
+        $variable = $this->get_variable($name);
+        if($variable !== null){
+            return $this->cast((int)$variable['type'],
+                $this->con->fetchColumn('SELECT `value` FROM `' . $this->prefix . $this->get_database((int)$variable['type']) . '` WHERE `space` = ? AND `variable_id` = ?',
+                [$this->space, $variable['id']]));
+        }else{
+            return $default;
+        }
+    }
+
+    function delete($name){
+        $variable = $this->get_variable($name);
+        if($variable !== null){
+            $this->con->execute('DELETE FROM `' . $this->prefix . 'variable` WHERE `space` = ? AND `name` = ?', [$this->space, $name]);
+            $this->con->execute('DELETE FROM `' . $this->prefix . $this->get_database($variable['type']) . '` WHERE `space` = ? AND `variable_id` = ?',
+                [$this->space, $variable['id']]);
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
